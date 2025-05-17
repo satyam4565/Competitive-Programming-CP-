1d5bdc11cf8675518496d24b790140c119a36e56
SatyamSingh <164022365+satyam4565@users.noreply.github.com>
2025-05-17 07:47:21 +0530

    commit

---
+++ CP-31 1000/A_Reverse_a_Substring.cpp
@@ -0,0 +1,72 @@
+#include "bits/stdc++.h"
+#define yup cout<<"YES"<<"\n"
+#define nope cout<<"NO"<<"\n"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define q_max priority_queue<int,vi>
+#define q_min priority_queue<int,vi,greater<int>>
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define frc(v,i,n) for(int i=0; i<(n); i++){cin>>v[i];}
+#define loop(i,a,n) for(int i=(a); i<(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int,int>
+using namespace std;
+int MOD=1e9+7;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+
+int printAns(int i,string s){
+    char ch = s[i];
+    for(int j=i+1;j<s.size();j++){
+        if(s[j]<ch){
+            return j+1;
+        }
+    }
+}
+
+void solve(){
+    int n;cin>>n;string s;cin>>s;
+    string str=s;
+    srt(str);
+    for(int i=0;i<n;i++){
+        if(s[i]>str[i]){
+            yup;
+            cout<<i+1<<" "<<printAns(i,s)<<"\n";
+            return;
+        }
+    }
+    nope;
+    
+    
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    solve();
+    return 0;
+}
+
+    
