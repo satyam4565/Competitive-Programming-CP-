7de1f52ac3579932141cc414fea3cffdebad665f
SatyamSingh <164022365+satyam4565@users.noreply.github.com>
2025-05-20 11:44:49 +0530

    commit

---
+++ CP - 31 900/B_01_Game.cpp
@@ -0,0 +1,68 @@
+#include "bits/stdc++.h"
+#define yup cout<<"DA"<<"\n"
+#define nope cout<<"NET"<<"\n"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define q_max priority_queue<int,vi>
+#define q_min priority_queue<int,vi,greater<int>>
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define frc(v,i,n) for(int i=0; i<(n); i++){cin>>v[i];}
+#define loop(i,a,n) for(int i=(a); i<(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int,int>
+using namespace std;
+int MOD=1e9+7;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+int powerOfTwo(int n) {if (n <= 0) return -1;if ((n & (n - 1)) == 0) return log2(n);else return -1;}
+
+void solve(){
+    string s;cin>>s;
+    int n = s.size();   
+    bool flag = false;
+    while(s.size()!=0){
+        auto it1 = s.find("01");
+        auto it2 = s.find("10");
+        if(it1 == string::npos && it2 == string::npos) break;
+        else if(it1 != string::npos) s.erase(it1,2);
+        else if(it2 != string::npos) s.erase(it2,2);
+        flag =  1 - flag;
+    }
+    flag ? yup : nope;  
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
