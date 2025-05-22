98db20137afc9e00fa147f5048fc32a9bb3a7445
SatyamSingh <164022365+satyam4565@users.noreply.github.com>
2025-05-22 11:53:10 +0530

    commit

---
+++ CP - 31 1400/B_Orac_and_Models.cpp
@@ -0,0 +1,59 @@
+#include "bits/stdc++.h"
+#define yup cout<<"YES"<<"\n"
+#define nope cout<<"NO"<<"\n"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define frc(v,i,n) for(int i=0; i<(n); i++){cin>>v[i];}
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())
+#define mxe(v)  *max_element(v.begin(),v.end())
+#define mne(v)  *min_element(v.begin(),v.end())
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+#define bin(x,y)  bitset<y>(x)
+using namespace std;
+int MOD=1e9+7;
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+
+
+void solve(){
+    int n;cin>>n;
+    vi v(n+1);for(int i=1;i<=n;i++) cin>>v[i];
+    vi dp(n+1,1);
+    for(int i=1;i<=n;i++){
+        for(int j=2*i;j<=n;j+=i){
+            if(v[j]>v[i]) dp[j] = max(dp[j],dp[i]+1);
+        }
+    }
+    cout<<mxe(dp)<<endl;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
